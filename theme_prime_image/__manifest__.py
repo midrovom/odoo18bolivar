1913c99b48c3f16029e08fdefe80db44a53ceb8b
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-07 09:45:11 -0500

    fix-update

--- theme_prime_image/__manifest__.py
+++ theme_prime_image/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Theme Prime Image',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Extensión visual para theme prime',
     'category': 'Theme/eCommerce',
     'depends': ['theme_prime'],
