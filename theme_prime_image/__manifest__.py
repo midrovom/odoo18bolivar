10ff36e46a4412ec9b53a478224e8294dca2fb6d
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-13 09:45:08 -0500

    fix-update

--- theme_prime_image/__manifest__.py
+++ theme_prime_image/__manifest__.py
@@ -6,17 +6,16 @@
     'depends': ['theme_prime'],
     'data': [
         'views/theme_shop_layout_views.xml',
-        #'views/snippet/banner_custom/snippet_banner_16.xml',´
         'views/snippet/banner_custom/snippet_banner_9_custom.xml',
         'views/snippet/subscribe_custom/snippet_subscribe.xml',
         'views/snippet/shop_offert_custom/s_shop_offer_custom.xml',
         'views/snippet/cover_custom/snippet_cover_custom.xml',
         'views/snippet/shop_offert_custom/shop_offer_6.xml',
         'views/snippet/clients_custom/clients_custom.xml',
+        'views/snippet/cover_custom/snippet_cover_6.xml',
     ],
     'assets': {
         'web.assets_frontend': [
-            #'theme_prime_image/static/src/js/s_banner.js',
         ],
     },
     'installable': True,
