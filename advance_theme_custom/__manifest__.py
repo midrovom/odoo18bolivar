4d903c2f8d6bb5b980aa751bbb6c444ef87beecb
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-14 10:01:29 -0500

    fix-update: ...

---
+++ advance_theme_custom/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Advance Theme Custom',
+    'version': '18.0.1.0.0',
+    'summary': 'Extensión de estilos',
+    'category': 'eCommerce',
+    'depends': ['website'],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'advance_theme_custom/static/src/scss/cover_custom/snippet_cover_custom.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
