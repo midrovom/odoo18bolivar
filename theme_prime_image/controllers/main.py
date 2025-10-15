fc730abf054e8628e042b4d955141ef8127c901d
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-15 17:57:18 -0500

    fix-update

--- theme_prime_image/controllers/main.py
+++ theme_prime_image/controllers/main.py
@@ -1,18 +1,19 @@
 from odoo import http
 from odoo.http import request
-from odoo.osv import expression
 from odoo.addons.theme_prime.controllers.main import ThemePrimeController
 
 class ThemePrimeImageController(ThemePrimeController):
 
     @http.route('/theme_prime/get_products_by_category', type='json', auth='public', website=True)
     def get_products_by_category(self, domain, fields=[], options={}, **kwargs):
-        final_domain = expression.AND([[('website_published', '=', True)], domain])
-        products = self._get_products(domain=final_domain, fields=fields, order=options.get('order', False), limit=options.get('limit', False), options=options)
+        # Ejecuta el controlador original
+        result = super().get_products_by_category(domain, fields, options, **kwargs)
 
-        for product in products:
+        # Añade atributos personalizados a cada producto
+        for product in result.get('products', []):
+            product_record = request.env['product.template'].sudo().browse(product['id'])
             attribute_data = []
-            for line in product.attribute_line_ids:
+            for line in product_record.attribute_line_ids:
                 if line.attribute_id.name != 'Marca':
                     values = []
                     for val in line.value_ids:
@@ -25,25 +26,6 @@ class ThemePrimeImageController(ThemePrimeController):
                         'attribute_id': {'name': line.attribute_id.name},
                         'value_ids': values,
                     })
-            product.attribute_line_ids_data = attribute_data  # campo virtual para JS
-
-        result = {
-            'products': [{
-                'id': p.id,
-                'name': p.name,
-                'list_price': p.list_price,
-                'attribute_line_ids': getattr(p, 'attribute_line_ids_data', []),
-            } for p in products],
-        }
-
-        result.update(self._get_shop_related_data(options))
-
-        if options.get('get_categories'):
-            domain = [('id', 'in', options.get('categoryIDs'))]
-            result['categories'] = self._get_categories(domain)
-
-        if options.get('get_brands'):
-            domain = [('id', 'in', options.get('categoryIDs'))]
-            result['categories'] = request.website._get_brands(domain).read(['name', 'id'])
+            product['attribute_line_ids'] = attribute_data
 
         return result
