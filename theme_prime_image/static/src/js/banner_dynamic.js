f52bfcaad451a1cf5ac8a3c18c2bdc821f27d7a9
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-21 14:55:24 -0500

    fix-update: ...

--- theme_prime_image/static/src/js/banner_dynamic.js
+++ theme_prime_image/static/src/js/banner_dynamic.js
@@ -1,19 +1,16 @@
+// import { registry } from '@web/core/registry';
+
+// const originalProductsData = registry.category('theme_prime_data').get('PRODUCTS_DATA');
+// const extendedProductsData = {
+//     ...originalProductsData,
+//     fields: [...originalProductsData.fields, 'attributes'], 
+// };
+
 /** @odoo-module **/
 
-import { registry } from '@web/core/registry';
-
-const originalProductsData = registry.category('theme_prime_data').get('PRODUCTS_DATA');
-const extendedProductsData = {
-    ...originalProductsData,
-    fields: [...originalProductsData.fields, 'attributes'], 
-};
-
-// Registramos de nuevo, forzando la sobreescritura
-registry.category('theme_prime_data').add('PRODUCTS_DATA', extendedProductsData, { force: true });
-
-
-
-
+import { registry } from "@web/core/registry";
 
 const brandCardRegistry = registry.category("theme_prime_brand_card_registry");
-brandCardRegistry.add("tp_brand_card_style_custom", { supportedActions: [] });
+
+// Añadimos el estilo 6, igual que los demás
+brandCardRegistry.add("tp_brand_card_style_6", { supportedActions: [] });
