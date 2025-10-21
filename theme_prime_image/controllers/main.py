67eec9d318db3c66b47d36bb9d882b86a02ebf7e
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-21 09:54:40 -0500

    fix-update: ...

--- theme_prime_image/controllers/main.py
+++ theme_prime_image/controllers/main.py
@@ -1,11 +1,15 @@
-class WebsiteProductsExtended(WebsiteProducts):
+from odoo import http
+from odoo.http import request
+from odoo.addons.theme_prime.controllers.main import ThemePrimeWebsiteSale
+
+class ThemePrimeWebsiteSaleExtended(ThemePrimeWebsiteSale):
 
     @http.route('/theme_prime/get_products_by_category', type='json', auth='public', website=True)
     def get_products_by_category(self, domain, fields=[], options={}, **kwargs):
         result = super().get_products_by_category(domain, fields=fields, options=options, **kwargs)
 
         for product in result.get('products', []):
-            pt = request.env['product.template'].browse(product['id'])
+            pt = request.env['product.template'].sudo().browse(product['id'])
             attributes = []
             for line in pt.attribute_line_ids:
                 attributes.append({
