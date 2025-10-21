18fb6b31e735c3a2182f3b1b141d52413197ac35
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-21 15:02:32 -0500

    fix-update: ...

--- theme_prime_image/static/src/js/banner_dynamic.js
+++ theme_prime_image/static/src/js/banner_dynamic.js
@@ -6,11 +6,11 @@
 //     fields: [...originalProductsData.fields, 'attributes'], 
 // };
 
+
+
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
 
 const brandCardRegistry = registry.category("theme_prime_brand_card_registry");
-
-// Añadimos el estilo 6, igual que los demás
 brandCardRegistry.add("tp_brand_card_style_6", { supportedActions: [] });
