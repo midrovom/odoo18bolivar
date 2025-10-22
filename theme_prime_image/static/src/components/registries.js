0232c8681dc63bcdab4b5a79f8378e37bf183403
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-22 16:04:49 -0500

    fix-update: ...

--- theme_prime_image/static/src/components/registries.js
+++ theme_prime_image/static/src/components/registries.js
@@ -15,3 +15,23 @@ megaMenuRegistry.add("s_tp_hierarchical_category_style_1", {
         "layout",   // nueva opcion
     ],
 });
+
+
+// Extender el snippet de mega menú
+const megaMenusRegistry = registry.category("theme_prime_mega_menus");
+
+megaMenusRegistry.add("s_tp_mega_menu_category_snippet", {
+    widgets: {
+        TpUiComponent: {
+            cardRegistry: "theme_prime_mega_menu_cards",
+            defaultVal: {
+                style: "s_tp_hierarchical_category_style_1",
+                layout: "slider",   
+                childOrder: "sequence",
+                productListing: "newArrived",
+                limit: 5,
+                activeActions: ["brand", "label", "count"],
+            },
+        },
+    },
+});
