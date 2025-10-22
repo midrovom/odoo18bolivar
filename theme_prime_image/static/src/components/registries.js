e356e69f6010a3607e4152fbeecac289c5fbcc53
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-22 16:28:14 -0500

    fix-update: ...

--- theme_prime_image/static/src/components/registries.js
+++ theme_prime_image/static/src/components/registries.js
@@ -2,18 +2,34 @@
 
 import { registry } from "@web/core/registry";
 
-const megaMenuRegistry = registry.category("theme_prime_mega_menu_cards");
+registry.category("theme_prime_mega_menu_cards")
+    .add("s_tp_hierarchical_category_style_1", {
+        supportedActions: [
+            "limit",
+            "brand",
+            "label",
+            "count",
+            "style",
+            "background",
+            "mode",   
+        ],
+    });
 
-megaMenuRegistry.add("s_tp_hierarchical_category_style_1", {
-    supportedActions: [
-        "limit",
-        "brand",
-        "label",
-        "count",
-        "style",
-        "background",
-        "mode",   // nueva opcion
-    ],
-});
+registry.category("theme_prime_mega_menus")
+    .add("s_tp_mega_menu_category_snippet", {
+        widgets: {
+            TpUiComponent: {
+                cardRegistry: "theme_prime_mega_menu_cards",
+                defaultVal: {
+                    style: "s_tp_hierarchical_category_style_1",
+                    mode: "slider",   
+                    childOrder: "sequence",
+                    productListing: "newArrived",
+                    limit: 5,
+                    activeActions: ["brand", "label", "count"],
+                },
+            },
+        },
+    });
 
 
