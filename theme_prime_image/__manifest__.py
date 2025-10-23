3075f99df3edfc9ef15f509f8c06eda90abaede5
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-23 14:52:37 -0500

    fix-update

--- theme_prime_image/__manifest__.py
+++ theme_prime_image/__manifest__.py
@@ -26,7 +26,7 @@
             
         ],
         'website.assets_wysiwyg': [
-            #'theme_prime_image/static/src/components/*'
+            'theme_prime_image/static/src/components/*'
             
         ],
     },
