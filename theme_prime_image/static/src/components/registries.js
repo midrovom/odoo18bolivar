f0fb72759bd7527fe54b67422cec7c44dbbec1ea
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-23 10:49:55 -0500

    fix-update: ...

--- theme_prime_image/static/src/components/registries.js
+++ theme_prime_image/static/src/components/registries.js
@@ -1,35 +1,10 @@
 /** @odoo-module **/
 
-import { registry } from "@web/core/registry";
+import { registry } from '@web/core/registry';
 
-registry.category("theme_prime_mega_menu_cards")
-    .add("s_tp_hierarchical_category_style_1", {
-        supportedActions: [
-            "limit",
-            "brand",
-            "label",
-            "count",
-            "style",
-            "background",
-            "mode",   
-        ],
+let PRODUCTS_ACTIONS = ['rating', 'quick_view', 'add_to_cart', 'comparison', 'wishlist', 'category_info', 'label'];
+
+registry.category('theme_prime_card_registry')
+    .add('s_card_style_9', {
+        supportedActions: [...new Set([...PRODUCTS_ACTIONS, ...['show_similar', 'colors']])]
     });
-
-registry.category("theme_prime_mega_menus")
-    .add("s_tp_mega_menu_category_snippet", {
-        widgets: {
-            TpUiComponent: {
-                cardRegistry: "theme_prime_mega_menu_cards",
-                defaultVal: {
-                    style: "s_tp_hierarchical_category_style_1",
-                    mode: "slider",   
-                    childOrder: "sequence",
-                    productListing: "newArrived",
-                    limit: 5,
-                    activeActions: ["brand", "label", "count"],
-                },
-            },
-        },
-    });
-
-
