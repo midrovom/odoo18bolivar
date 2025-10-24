e40049e0f82ff1facd3049b74bdb41648e942c5b
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-24 12:43:09 -0500

    fix-update

--- theme_prime_image/static/src/js/new_registries.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module **/
-
-import { qweb } from "web.core";
-
-// Esto obliga a Odoo a incluir el template en los assets compilados
-qweb.add_template(require('theme_prime_image.cards'));
