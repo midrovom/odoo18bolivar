34d4a6e4bff72f8ba9f8931b2b3cc10fa674dcad
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-24 17:08:35 -0500

    fix-update

--- theme_prime_image/controllers/main.py
+++ theme_prime_image/controllers/main.py
@@ -1,34 +1,24 @@
-# from odoo import http
-# from odoo.http import request
-# from odoo.addons.theme_prime.controllers.main import ThemePrimeMainClass
+from odoo.addons.theme_prime.controllers.main import ThemePrimeMainClass
+from odoo.http import request
 
 
-# class ThemePrimeMainClassExtended(ThemePrimeMainClass):
+class ThemePrimeMainClassExtended(ThemePrimeMainClass):
 
-#     def _prepare_product_data(self, products, fields, pricelist, options=None):
-#         # Asegurar que los campos necesarios para atributos estén incluidos
-#         fields = list(set(fields or []))
-#         fields += [
-#             'attribute_line_ids',
-#             'attribute_line_ids.attribute_id',
-#             'attribute_line_ids.value_ids',
-#             'attribute_line_ids.value_ids.name',
-#             'attribute_line_ids.value_ids.dr_image',
-#         ]
+    def _prepare_product_data(self, products, fields, pricelist, options=None):
+        fields = list(set(fields or []))
+        fields += ['attribute_line_ids']  # Solo este campo es necesario para acceder desde el objeto
 
-#         # Llamar al método original para conservar toda la lógica base
-#         result = super()._prepare_product_data(products, fields, pricelist, options)
+        result = super()._prepare_product_data(products, fields, pricelist, options)
 
-#         # Agregar atributos personalizados a cada producto
-#         for res_product, product in zip(result, products):
-#             res_product['attributes'] = []
-#             for line in product.attribute_line_ids:
-#                 if line.attribute_id.name != 'Marca':
-#                     for val in line.value_ids:
-#                         res_product['attributes'].append({
-#                             'id': val.id,
-#                             'name': val.name,
-#                             'image': val.dr_image,
-#                         })
+        for res_product, product in zip(result, products):
+            res_product['attributes'] = []
+            for line in product.attribute_line_ids:
+                if line.attribute_id.name != 'Marca':
+                    for val in line.value_ids:
+                        res_product['attributes'].append({
+                            'id': val.id,
+                            'name': val.name,
+                            'image': val.dr_image,
+                        })
 
-#         return result
+        return result
