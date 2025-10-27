7225e3c22662b2d321ab01ef8b5107931fb8d9d4
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-27 15:16:28 -0500

    fix-update

--- theme_prime_image/static/src/components/registries.js
+++ theme_prime_image/static/src/components/registries.js
@@ -9,9 +9,14 @@ let SELECTOR_DATA = { TpRecordSelector: { ...PRODUCTS_DATA, defaultVal: { select
 let EXTRA_OPTIONS = { TpExtraOpts: { startDate: '', endDate: '', priceList: '*' } };
 let CATEGORY_SELECTOR_DATA = { TpRecordSelector: { ...CATEGORIES_DATA, defaultVal: { selectionType: 'manual', recordsIDs: [], model: 'product.public.category'}}};
 
-
+//Registro para snippet product card 
+//web
 registry.category('theme_prime_card_registry')
     .add('s_card_style_9', { supportedActions: [...new Set([...PRODUCTS_ACTIONS, ...['show_similar']])]})
+//mobile
+registry.category('theme_prime_mobile_card_registry')
+    .add('s_mobile_card_style_3', {supportedActions: []})
+
 
 registry.category('theme_prime_mega_menu_cards')
     .add('s_tp_hierarchical_category_style_12', {supportedActions: ['limit', 'brand', 'label', 'count', 'style', 'background']});
