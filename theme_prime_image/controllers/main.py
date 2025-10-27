b4ea3ec3e951b26c4302cd906a17c28bcccd09a8
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-27 15:02:05 -0500

    fix-update

--- theme_prime_image/controllers/main.py
+++ theme_prime_image/controllers/main.py
@@ -2,11 +2,32 @@ from odoo.addons.theme_prime.controllers.main import ThemePrimeMainClass
 from odoo.http import request
 
 
+# class ThemePrimeMainClassExtended(ThemePrimeMainClass):
+
+#     def _prepare_product_data(self, products, fields, pricelist, options=None):
+#         fields = list(set(fields or []))
+#         fields += ['attribute_line_ids']  # Solo este campo es necesario para acceder desde el objeto
+
+#         result = super()._prepare_product_data(products, fields, pricelist, options)
+
+#         for res_product, product in zip(result, products):
+#             res_product['attributes'] = []
+#             for line in product.attribute_line_ids:
+#                 if line.attribute_id.name != 'Marca':
+#                     for val in line.value_ids:
+#                         res_product['attributes'].append({
+#                             'id': val.id,
+#                             'name': val.name,
+#                             'image': val.dr_image,
+#                         })
+
+#         return result
+
 class ThemePrimeMainClassExtended(ThemePrimeMainClass):
 
     def _prepare_product_data(self, products, fields, pricelist, options=None):
         fields = list(set(fields or []))
-        fields += ['attribute_line_ids']  # Solo este campo es necesario para acceder desde el objeto
+        fields += ['attribute_line_ids']
 
         result = super()._prepare_product_data(products, fields, pricelist, options)
 
@@ -18,7 +39,7 @@ class ThemePrimeMainClassExtended(ThemePrimeMainClass):
                         res_product['attributes'].append({
                             'id': val.id,
                             'name': val.name,
-                            'image': val.dr_image,
+                            'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
                         })
 
         return result
