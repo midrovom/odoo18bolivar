e84a38126faf73ee133b12d6c2b439a35fdab2b2
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-28 15:07:55 -0500

    fix-update: ...

--- theme_prime_image/static/src/js/s_tp_hierarchical_category_style_12.js
+++ theme_prime_image/static/src/js/s_tp_hierarchical_category_style_12.js
@@ -1,14 +1,24 @@
-/** @odoo-module **/
+odoo.define('theme_prime_image.category_carousel', function (require) {
+    'use strict';
 
-function scrollSlider(direction) {
-    const slider = document.getElementById('tpCategorySlider');
-    const scrollAmount = 220;
-    if (slider) {
-        slider.scrollBy({
-            left: direction * scrollAmount,
-            behavior: 'smooth'
-        });
-    }
-}
+    var publicWidget = require('web.public.widget');
 
-window.scrollSlider = scrollSlider;
+    publicWidget.registry.CategoryCarousel = publicWidget.Widget.extend({
+        selector: '.owl-carousel.droggol_product_slider',
+        start: function () {
+            this.$el.owlCarousel({
+                items: 4,
+                margin: 10,
+                loop: true,
+                nav: true,
+                dots: false,
+                responsive: {
+                    0: { items: 1 },
+                    576: { items: 2 },
+                    768: { items: 3 },
+                    992: { items: 4 }
+                }
+            });
+        },
+    });
+});
