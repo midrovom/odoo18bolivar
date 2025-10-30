5fa904a1cc1b88bff68893916de158c9353c9df7
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-30 11:22:43 -0500

    fix-update: ...

--- theme_prime_image/static/src/components/registries.js
+++ theme_prime_image/static/src/components/registries.js
@@ -19,5 +19,9 @@ registry.category('theme_prime_card_registry')
 
 
 registry.category('theme_prime_mega_menu_cards')
-    .add('s_tp_hierarchical_category_style_12', {supportedActions: ['limit', 'brand', 'label', 'count', 'style', 'background']});
-    
+    .add('s_tp_hierarchical_category_style_12', {
+        supportedActions: ['limit', 'brand', 'label', 'count', 'style', 'background'],
+        defaultVal: {
+            mode: 'slider'  
+        }
+    });
