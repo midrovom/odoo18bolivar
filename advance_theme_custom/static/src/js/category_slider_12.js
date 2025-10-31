f26cb92eb57e737056fa503a50becb0f537b1e92
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-31 11:57:10 -0500

    fix-update

--- advance_theme_custom/static/src/js/category_slider_12.js
+++ advance_theme_custom/static/src/js/category_slider_12.js
@@ -1,41 +1,32 @@
-/** @odoo-module **/
+odoo.define('advance_theme_custom.owl_snippet', function (require) {
+    'use strict';
 
-import publicWidget from "@web/legacy/js/public/public_widget";
-import { localization } from "@web/core/l10n/localization";
+    const publicWidget = require('web.public.widget');
 
-publicWidget.registry.TpCategorySlider12 = publicWidget.Widget.extend({
-    selector: '.tp-category-slider-12',
-    jsLibs: ['/theme_prime/static/lib/OwlCarousel2-2.3.4/owl.carousel.js'],
-
-    on_attach_callback: function () {
-        console.log("TpCategorySlider12 activo (snippet dinámico)");
-        this._initializeOWL();
-    },
-
-    _initializeOWL: function () {
-        const $owlSlider = this.$el;
-
-        if ($owlSlider.hasClass("owl-loaded")) {
-            $owlSlider.trigger("refresh.owl.carousel");
-            return;
-        }
-
-        $owlSlider.owlCarousel({
-            dots: false,
-            margin: 10,
-            loop: true,
-            nav: true,
-            autoplay: true,
-            autoplayTimeout: 3000,
-            autoplayHoverPause: true,
-            rewind: true,
-            rtl: localization.direction === 'rtl',
-            responsive: {
-                0: { items: 2 },
-                576: { items: 4 },
-                768: { items: 6 },
-                992: { items: 8 }
+    publicWidget.registry.OwlCarouselSnippet = publicWidget.Widget.extend({
+        selector: '.tp-category-slider-12',
+        start: function () {
+            if (this.$el.hasClass('owl-loaded')) {
+                this.$el.trigger('destroy.owl.carousel');
+                this.$el.removeClass('owl-loaded');
+                this.$el.find('.owl-stage-outer').children().unwrap();
             }
-        });
-    },
+
+            this.$el.owlCarousel({
+                items: 8,
+                margin: 10,
+                loop: true,
+                nav: true,
+                dots: false,
+                responsive: {
+                    0: { items: 2 },
+                    576: { items: 4 },
+                    768: { items: 6 },
+                    992: { items: 8 }
+                }
+            });
+
+            return this._super.apply(this, arguments);
+        },
+    });
 });
