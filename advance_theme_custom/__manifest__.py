5b90e6c6af333fcd910cf4732ea55be5a2ac5db4
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-05 14:48:09 -0500

    fix-update: ...

--- advance_theme_custom/__manifest__.py
+++ advance_theme_custom/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'eCommerce',
     'depends': ['website', 'website_sale'],
     'data': [
-        'views/snippets/dynamic_snippet/register.xml',
+        #'views/snippets/dynamic_snippet/register.xml',
         #'views/snippets/dynamic_snippet/dynamic_snippet_custom.xml',
     ],
     'assets': {
