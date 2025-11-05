f60ca996f0f08b3212c42fabd2a3565c804bf238
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-05 16:06:44 -0500

    fix-update: ...

---
+++ advance_theme_custom/static/src/js/dynamic_snippet_categories.js
@@ -0,0 +1,29 @@
+import { DynamicSnippetCarousel } from "@website/snippets/s_dynamic_snippet_carousel/dynamic_snippet_carousel";
+import { registry } from "@web/core/registry";
+
+export class DynamicSnippetCategories extends DynamicSnippetCarousel {
+    static selector = ".s_dynamic_snippet_categories";
+
+    getSearchDomain() {
+        const searchDomain = [];
+        const categoryIds = this.el.dataset.categoryIds;
+        if (categoryIds) {
+            const parsed = JSON.parse(categoryIds);
+            if (parsed.length) {
+                searchDomain.push(["id", "in", parsed.map(c => c.id)]);
+            }
+        }
+        return searchDomain;
+    }
+
+    getRpcParameters() {
+        return Object.assign(super.getRpcParameters(...arguments), {
+            categoryIds: this.el.dataset.categoryIds,
+        });
+    }
+}
+
+registry.category("public.interactions").add("advance_theme_custom.dynamic_snippet_categories", DynamicSnippetCategories);
+registry.category("public.interactions.edit").add("advance_theme_custom.dynamic_snippet_categories", {
+    Interaction: DynamicSnippetCategories,
+});
---
+++ advance_theme_custom/static/src/js/dynamic_snippet_categories_option_plugin.js
@@ -0,0 +1,55 @@
+import { DYNAMIC_SNIPPET_CAROUSEL } from "@website/builder/plugins/options/dynamic_snippet_carousel_option_plugin";
+import { setDatasetIfUndefined } from "@website/builder/plugins/options/dynamic_snippet_option_plugin";
+import { Plugin } from "@html_editor/plugin";
+import { withSequence } from "@html_editor/utils/resource";
+import { registry } from "@web/core/registry";
+
+class DynamicSnippetCategoriesOptionPlugin extends Plugin {
+    static id = "dynamicSnippetCategoriesOption";
+    static dependencies = ["dynamicSnippetCarouselOption"];
+    selector = ".s_dynamic_snippet_categories";
+    modelNameFilter = "product.public.category";
+
+    resources = {
+        builder_options: withSequence(DYNAMIC_SNIPPET_CAROUSEL, {
+            OptionComponent: null, // se peude crear un componente si se requiere UI personalizada
+            props: {
+                modelNameFilter: this.modelNameFilter,
+                fetchCategories: this.fetchCategories.bind(this),
+            },
+            selector: this.selector,
+        }),
+        on_snippet_dropped_handlers: this.onSnippetDropped.bind(this),
+    };
+
+    setup() {
+        this.categories = undefined;
+    }
+
+    async onSnippetDropped({ snippetEl }) {
+        if (snippetEl.matches(this.selector)) {
+            setDatasetIfUndefined(snippetEl, "categoryIds", "[]");
+            setDatasetIfUndefined(snippetEl, "numberOfRecords", "8");
+            setDatasetIfUndefined(snippetEl, "carouselInterval", "5000");
+        }
+    }
+
+    async fetchCategories() {
+        if (!this.categories) {
+            const websiteDomain = [
+                "|",
+                ["website_id", "=", false],
+                ["website_id", "=", this.services.website.currentWebsite.id],
+            ];
+            this.categories = this.services.orm.searchRead(
+                "product.public.category",
+                websiteDomain,
+                ["id", "name"],
+                { order: "name asc" }
+            );
+        }
+        return this.categories;
+    }
+}
+
+registry.category("website-plugins").add(DynamicSnippetCategoriesOptionPlugin.id, DynamicSnippetCategoriesOptionPlugin);
