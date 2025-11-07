edd86835c8cf892a9b88dd0c8001fd96f2fc801d
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-07 16:45:25 -0500

    fix-update

--- advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/option.js
+++ advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/option.js
@@ -2,89 +2,48 @@
 
 import options from "@web_editor/js/editor/snippets.options";
 import s_dynamic_snippet_carousel_options from "@website/snippets/s_dynamic_snippet_carousel/options";
-
 import wUtils from "@website/js/utils";
 
-const alternativeSnippetRemovedOptions = [
-    'filter_opt', 'product_category_opt', 'product_tag_opt', 'product_names_opt',
-]
-
-const dynamicSnippetProductsOptions = s_dynamic_snippet_carousel_options.extend({
+const dynamicSnippetCategoriesOptions = s_dynamic_snippet_carousel_options.extend({
 
     /**
-     *
      * @override
      */
     init: function () {
         this._super.apply(this, arguments);
-        this.modelNameFilter = 'product.product';
-        // Directly calling $() will not work in this case since we are querying something
-        // in an iframe
-        const productTemplateId = this.$target.closest("#wrapwrap").find("input.product_template_id");
-        this.hasProductTemplateId = productTemplateId.val();
-        if (!this.hasProductTemplateId) {
-            this.contextualFilterDomain.push(['product_cross_selling', '=', false]);
-        }
+        this.modelNameFilter = 'product.public.category';
         this.productCategories = {};
-        this.isAlternativeProductSnippet = this.$target.hasClass('o_wsale_alternative_products');
-
         this.orm = this.bindService("orm");
     },
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
 
     /**
      * @private
-     * @override
      */
-     _computeWidgetVisibility(widgetName, params) {
-        if (this.isAlternativeProductSnippet && alternativeSnippetRemovedOptions.includes(widgetName)) {
-            return false;
-        }
-        return this._super(...arguments);
-    },
-    /**
-     * Fetches product categories.
-     * @private
-     * @returns {Promise}
-     */
-    _fetchProductCategories: function () {
+    _fetchCategories: function () {
         return this.orm.searchRead("product.public.category", wUtils.websiteDomain(this), ["id", "name"]);
     },
+
     /**
-     *
      * @override
      * @private
      */
     _renderCustomXML: async function (uiFragment) {
         await this._super.apply(this, arguments);
-        await this._renderProductCategorySelector(uiFragment);
+        await this._renderCategorySelector(uiFragment);
     },
+
     /**
-     * Renders the product categories option selector content into the provided uiFragment.
      * @private
-     * @param {HTMLElement} uiFragment
      */
-    _renderProductCategorySelector: async function (uiFragment) {
-        const productCategories = await this._fetchProductCategories();
-        for (let index in productCategories) {
-            this.productCategories[productCategories[index].id] = productCategories[index];
+    _renderCategorySelector: async function (uiFragment) {
+        const categories = await this._fetchCategories();
+        for (let category of categories) {
+            this.productCategories[category.id] = category;
         }
-        const productCategoriesSelectorEl = uiFragment.querySelector('[data-name="product_category_opt"]');
-        return this._renderSelectUserValueWidgetButtons(productCategoriesSelectorEl, this.productCategories);
-    },
-    /**
-     * @override
-     * @private
-     */
-    _setOptionsDefaultValues: function () {
-        this._setOptionValue('productCategoryId', 'all');
-        this._setOptionValue('showVariants', true);
-        this._super.apply(this, arguments);
+        const categorySelectorEl = uiFragment.querySelector('[data-name="category_opt"]');
+        return this._renderSelectUserValueWidgetButtons(categorySelectorEl, this.productCategories);
     },
 });
 
-options.registry.dynamic_snippet_products = dynamicSnippetProductsOptions;
-
-export default dynamicSnippetProductsOptions;
+options.registry.dynamic_snippet_categories = dynamicSnippetCategoriesOptions;
+export default dynamicSnippetCategoriesOptions;
