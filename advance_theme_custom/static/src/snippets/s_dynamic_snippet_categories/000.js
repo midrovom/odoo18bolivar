2a17706308d2122706393f4f6486fd0ec03ebf50
midrovom <midrovo@callphoneecuador.com>
2025-11-07 10:17:44 -0500

    fix-update: update

--- advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
+++ advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
@@ -1,27 +1,57 @@
 /** @odoo-module **/
 
-import { DynamicSnippet } from "@website/s_dynamic_snippet/000";
+import publicWidget from "@web/legacy/js/public/public_widget";
+import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
+import { rpc } from "@web/core/network/rpc";
 
-export class DynamicSnippetCategories extends DynamicSnippet {
+const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
+    selector: '.s_dynamic_snippet_categories',
 
+    /**
+     * Build the dynamic domain to search categories
+     *
+     * @override
+     */
+    _getSearchDomain() {
+        const domain = this._super(...arguments);
+
+        // Recoger opción desde dataset (si activaste checkbox Only Root)
+        const onlyRoot = this.el.dataset.onlyRoot === "true";
+        if (onlyRoot) {
+            domain.push(["parent_id", "=", false]);
+        }
+
+        return domain;
+    },
+
+    /**
+     * RPC to the backend, replace products by categories
+     *
+     * @override
+     */
     async _fetchData() {
-        const onlyRoot = this.props.onlyRoot === "true";
-        const domain = onlyRoot ? [["parent_id", "=", false]] : [];
+        const domain = this._getSearchDomain();
+        return rpc("/website/snippet/categories", { domain });
+    },
 
-        return this.rpc("/website/snippet/categories", { domain });
-    }
-
+    /**
+     * Render categories into the carousel
+     *
+     * @override
+     */
     _renderData(categories) {
-        const container = this.el.querySelector(".carousel-inner .carousel-item .row");
-        container.innerHTML = "";
+        const $container = this.$el.find('.carousel-item.active .row');
+        $container.empty();
 
         for (const cat of categories) {
-            container.insertAdjacentHTML("beforeend", `
+            $container.append(`
                 <div class="d-flex flex-grow-0 flex-shrink-0 col-3">
                     <div class="o_carousel_product_card bg-transparent w-100 card border-0 text-center">
                         <a class="stretched-link" href="${cat.url}">
                             <div class="overflow-hidden rounded">
-                                <img src="${cat.image}" class="card-img-top"/>
+                                <img class="card-img-top o_img_product_square o_img_product_cover h-auto"
+                                     loading="lazy"
+                                     src="${cat.image}"/>
                             </div>
                         </a>
                         <div class="o_carousel_product_card_body p-3">
@@ -31,7 +61,9 @@ export class DynamicSnippetCategories extends DynamicSnippet {
                 </div>
             `);
         }
-    }
-}
+    },
+});
 
-DynamicSnippet.registry['dynamic_snippet_categories'] = DynamicSnippetCategories;
+publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
+
+export default DynamicSnippetCategories;
