c4a97d4ec955f2f85646e5403e48218bf4c2b8ea
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-10 15:46:28 -0500

    fix-update

--- advance_theme_custom/models/product_template.py
+++ advance_theme_custom/models/product_template.py
@@ -4,5 +4,4 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     def _get_related_category_filter(self):
-        # Devuelve el ID del filtro dinámico de categorías relacionadas
         return self.env.ref('advance_theme_custom.dynamic_filter_related_categories').id
