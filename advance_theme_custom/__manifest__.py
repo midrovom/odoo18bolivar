018ab38145e72c045c6165b891129c3c7497ed1e
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-10 11:24:07 -0500

    fix-update

--- advance_theme_custom/__manifest__.py
+++ advance_theme_custom/__manifest__.py
@@ -7,7 +7,7 @@
     'data': [
         'data/data.xml',
         'data/product_public_categ_data.xml',
-        #'views/templates.xml',
+        'views/templates.xml',
         # 'views/snippets/dynamic_snippet_custom.xml',
         'views/snippets/snippets.xml',
         'views/snippets/s_dynamic_snippet_categories.xml',
