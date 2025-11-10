a2e404de7c64a0ef628457c953875504de5e0e63
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-10 15:11:52 -0500

    fix-update

--- advance_theme_custom/models/product_template.py
+++ advance_theme_custom/models/product_template.py
@@ -1,9 +1,8 @@
-from odoo import api, models
+from odoo import models
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    @api.model
     def _get_related_category_filter(self):
         # Devuelve el ID del filtro dinámico de categorías relacionadas
         return self.env.ref('advance_theme_custom.dynamic_filter_related_categories').id
