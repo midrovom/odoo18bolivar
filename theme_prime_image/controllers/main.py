31a9adeca35c381145f74dfabf59613a1144bab1
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-11 17:48:23 -0500

    fix-update

--- theme_prime_image/controllers/main.py
+++ theme_prime_image/controllers/main.py
@@ -6,13 +6,14 @@ class ThemePrimeMainClassExtended(ThemePrimeMainClass):
     def _prepare_product_data(self, products, fields, pricelist, options=None):
         fields = list(set(fields or []))
         fields += ['attribute_line_ids']
-
         result = super()._prepare_product_data(products, fields, pricelist, options)
 
+        # Recorremos los productos y filtramos atributos
         for res_product, product in zip(result, products):
             res_product['attributes'] = []
             for line in product.attribute_line_ids:
-                if line.attribute_id.name != 'Marca':
+                # Solo atributos con el check activado y que no sean "Marca"
+                if line.attribute_id.attribute_custom and line.attribute_id.name != 'Marca':
                     for val in line.value_ids:
                         res_product['attributes'].append({
                             'id': val.id,
@@ -21,3 +22,25 @@ class ThemePrimeMainClassExtended(ThemePrimeMainClass):
                         })
 
         return result
+
+
+# class ThemePrimeMainClassExtended(ThemePrimeMainClass):
+
+#     def _prepare_product_data(self, products, fields, pricelist, options=None):
+#         fields = list(set(fields or []))
+#         fields += ['attribute_line_ids']
+
+#         result = super()._prepare_product_data(products, fields, pricelist, options)
+
+#         for res_product, product in zip(result, products):
+#             res_product['attributes'] = []
+#             for line in product.attribute_line_ids:
+#                 if line.attribute_id.name != 'Marca':
+#                     for val in line.value_ids:
+#                         res_product['attributes'].append({
+#                             'id': val.id,
+#                             'name': val.name,
+#                             'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
+#                         })
+
+#         return result
