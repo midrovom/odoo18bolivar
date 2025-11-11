05902161afcdc19b61ad48963f59205194894954
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-11 18:05:41 -0500

    fix-update

--- advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
+++ advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
@@ -1,99 +1,19 @@
 /** @odoo-module **/
-
 import publicWidget from "@web/legacy/js/public/public_widget";
 import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
 
 const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
-    selector: '.s_dynamic_snippet_categories',
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Gets the parent category search domain
-     *
-     * @private
-     */
-    _getParentCategoryDomain() {
-        const searchDomain = [];
-        let parentCategoryId = this.$el.get(0).dataset.parentCategoryId;
-        if (parentCategoryId && parentCategoryId !== 'all') {
-            if (parentCategoryId === 'current') {
-                // Si el dataset dice "current", intentamos obtener la categoría actual
-                const categoryField = document.querySelector("#category_details .category_id");
-                if (categoryField) {
-                    parentCategoryId = parseInt(categoryField.value);
-                }
-            }
-            if (parentCategoryId) {
-                searchDomain.push(['parent_id', '=', parseInt(parentCategoryId)]);
-            }
-        }
-        return searchDomain;
-    },
-
-    /**
-     * Gets the tag search domain
-     *
-     * @private
-     */
-    _getTagSearchDomain() {
-        const searchDomain = [];
-        let categoryTagIds = this.$el.get(0).dataset.categoryTagIds;
-        categoryTagIds = categoryTagIds ? JSON.parse(categoryTagIds) : [];
-        if (categoryTagIds.length) {
-            searchDomain.push(['tag_ids', 'in', categoryTagIds.map(tag => tag.id)]);
-        }
-        return searchDomain;
-    },
-
-    /**
-     * Gets the name search domain
-     *
-     * @private
-     */
-    _getNameSearchDomain() {
-        const searchDomain = [];
-        const categoryNames = this.$el.get(0).dataset.categoryNames;
-        if (categoryNames) {
-            for (const name of categoryNames.split(',')) {
-                if (!name.length) {
-                    continue;
-                }
-                if (searchDomain.length) {
-                    searchDomain.unshift('|');
-                }
-                searchDomain.push(['name', 'ilike', name]);
-            }
-        }
-        return searchDomain;
-    },
-
-    /**
-     * Override: build the full search domain for categories
-     *
-     * @override
-     * @private
-     */
-    _getSearchDomain: function () {
-        const searchDomain = this._super.apply(this, arguments);
-        searchDomain.push(...this._getParentCategoryDomain());
-        searchDomain.push(...this._getTagSearchDomain());
-        searchDomain.push(...this._getNameSearchDomain());
-        if (this.el.dataset.onlyWithImage === 'true') {
-            searchDomain.push(['image_1920', '!=', false]);
-        }
-        return searchDomain;
-    },
+    selector: ".s_dynamic_snippet_categories",
 
     /**
      * @override
      * @private
      */
     _getMainPageUrl() {
-        return "/shop/categories";
+        return "/shop";
     },
+
+
 });
 
 publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
--- advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/option.js
+++ advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/option.js
@@ -3,70 +3,20 @@
 import options from "@web_editor/js/editor/snippets.options";
 import s_dynamic_snippet_carousel_options from "@website/snippets/s_dynamic_snippet_carousel/options";
 
-import wUtils from "@website/js/utils";
-
-const dynamicSnippetCategoriesOptions = s_dynamic_snippet_carousel_options.extend({
-
+const dynamicSnippetCategoryOptions = s_dynamic_snippet_carousel_options.extend({
     /**
      * @override
      */
-    init: function () {
+
+    // Metodo override que permite definir el filtro del modelo especifico que se crea en ir.filters model_id
+
+    init() {
         this._super.apply(this, arguments);
-        // El modelo sobre el que filtramos ahora es categorías públicas
         this.modelNameFilter = 'product.public.category';
-        this.categoryParents = {};
-        this.orm = this.bindService("orm");
     },
 
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Fetches parent categories.
-     * @private
-     * @returns {Promise}
-     */
-    _fetchParentCategories: function () {
-        return this.orm.searchRead("product.public.category", wUtils.websiteDomain(this), ["id", "name"]);
-    },
-
-    /**
-     * @override
-     * @private
-     */
-    _renderCustomXML: async function (uiFragment) {
-        await this._super.apply(this, arguments);
-        await this._renderParentCategorySelector(uiFragment);
-    },
-
-    /**
-     * Renders the parent categories option selector content into the provided uiFragment.
-     * @private
-     * @param {HTMLElement} uiFragment
-     */
-    _renderParentCategorySelector: async function (uiFragment) {
-        const parentCategories = await this._fetchParentCategories();
-        for (let index in parentCategories) {
-            this.categoryParents[parentCategories[index].id] = parentCategories[index];
-        }
-        const parentCategorySelectorEl = uiFragment.querySelector('[data-name="parent_category_opt"]');
-        if (parentCategorySelectorEl) {
-            return this._renderSelectUserValueWidgetButtons(parentCategorySelectorEl, this.categoryParents);
-        }
-    },
-
-    /**
-     * @override
-     * @private
-     */
-    _setOptionsDefaultValues: function () {
-        this._setOptionValue('parentCategoryId', 'all');
-        this._setOptionValue('onlyWithImage', false);
-        this._super.apply(this, arguments);
-    },
 });
 
-options.registry.dynamic_snippet_categories = dynamicSnippetCategoriesOptions;
+options.registry.dynamic_snippet_categories = dynamicSnippetCategoryOptions;
 
-export default dynamicSnippetCategoriesOptions;
+export default dynamicSnippetCategoryOptions;
