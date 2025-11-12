1baf76265cd972cadefecb0c59bd4c4c453815bb
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-12 08:57:33 -0500

    fix-update

--- advance_theme_custom/models/__init__.py
+++ advance_theme_custom/models/__init__.py
@@ -1,3 +1,3 @@
-#from . import product_attribute
-#from . import product_public_category
-#from . import website_snippet_filter
+from . import product_attribute
+from . import product_public_category
+from . import website_snippet_filter
