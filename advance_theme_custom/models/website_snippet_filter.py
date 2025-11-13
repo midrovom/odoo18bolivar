c008287b14226aad847b8a74b0c626ca8a8aeee9
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-13 16:30:36 -0500

    fix-update

--- advance_theme_custom/models/website_snippet_filter.py
+++ advance_theme_custom/models/website_snippet_filter.py
@@ -45,20 +45,3 @@ class WebsiteSnippetFilter(models.Model):
 
         return dynamic_filter.with_context()._filter_records_to_values(categories, is_sample=False)
 
-    # Herencia de filtros de productos recientemente visitados
-    def _get_products_latest_viewed(self, website, limit, domain, **kwargs):
-        products = super()._get_products_latest_viewed(website, limit, domain, **kwargs)
-
-        # Filtrar productos que NO tengan atributos con dr_is_brand=True o attribute_custom=True
-        filtered_products = self.env['product.product']
-        for product in products:
-            exclude = False
-            for line in product.product_tmpl_id.attribute_line_ids:
-                attr = line.attribute_id
-                if attr.dr_is_brand or attr.attribute_custom:
-                    exclude = True
-                    break
-            if not exclude:
-                filtered_products |= product
-
-        return filtered_products
