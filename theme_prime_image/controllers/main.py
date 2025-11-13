35c390f9ca1b65514b29df30b29991a81fe990d3
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-13 15:20:12 -0500

    fix-update

--- theme_prime_image/controllers/main.py
+++ theme_prime_image/controllers/main.py
@@ -30,32 +30,6 @@ class ThemePrimeMainClassExtended(ThemePrimeMainClass):
 
         return result
 
-# from odoo.addons.theme_prime.controllers.main import ThemePrimeMainClass
-# from odoo.http import request
-
-# class ThemePrimeMainClassExtended(ThemePrimeMainClass):
-
-#     def _prepare_product_data(self, products, fields, pricelist, options=None):
-#         fields = list(set(fields or []))
-#         fields += ['attribute_line_ids']
-#         result = super()._prepare_product_data(products, fields, pricelist, options)
-
-#         # Recorremos los productos y filtramos atributos
-#         for res_product, product in zip(result, products):
-#             res_product['attributes'] = []
-#             for line in product.attribute_line_ids:
-#                 # Solo atributos con el check activado y que no sean "Marca"
-#                 if line.attribute_id.attribute_custom and line.attribute_id.name != 'Marca':
-#                     for val in line.value_ids:
-#                         res_product['attributes'].append({
-#                             'id': val.id,
-#                             'name': val.name,
-#                             'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
-#                         })
-
-#         return result
-
-
 # class ThemePrimeMainClassExtended(ThemePrimeMainClass):
 
 #     def _prepare_product_data(self, products, fields, pricelist, options=None):
