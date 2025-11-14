fba4ba69c0a99f5b9b33cec2b8912ddf75cebe31
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-14 09:01:33 -0500

    fix-update

--- advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
+++ advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 import publicWidget from "@web/legacy/js/public/public_widget";
 import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
+import { utils as uiUtils } from "@web/core/ui/ui_service";
 
 const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
     selector: ".s_dynamic_snippet_categories",
@@ -13,9 +14,45 @@ const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
         return "/shop";
     },
 
+    /**
+     * @override
+     * @private
+     */
+    _getQWebRenderOptions: function () {
+        const options = this._super.apply(this, arguments);
 
+        options.rowPerSlide = parseInt(uiUtils.isSmall() ? 4 : this.el.dataset.rowPerSlide || 4);
+
+        return options;
+    },
 });
 
 publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
 
 export default DynamicSnippetCategories;
+
+
+
+
+
+// /** @odoo-module **/
+// import publicWidget from "@web/legacy/js/public/public_widget";
+// import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
+
+// const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
+//     selector: ".s_dynamic_snippet_categories",
+
+//     /**
+//      * @override
+//      * @private
+//      */
+//     _getMainPageUrl() {
+//         return "/shop";
+//     },
+
+
+// });
+
+// publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
+
+// export default DynamicSnippetCategories;
