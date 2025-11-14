f4d1137fcc84b78a23fc611b837fc9e1284c0f07
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-14 16:11:00 -0500

    fix-update: ...

--- advance_theme_custom/models/product_template.py
+++ advance_theme_custom/models/product_template.py
@@ -1,18 +1,16 @@
-from odoo.http import request
 from odoo import models
+from odoo.http import request
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     def _get_computed_product_price(self, product, product_data, price_public_visibility, visibility_label, currency_id):
-        FieldMonetary = request.env['ir.qweb.field.monetary']
-        monetary_options = {'display_currency': currency_id}
-
         res = super()._get_computed_product_price(
             product, product_data, price_public_visibility, visibility_label, currency_id
         )
+        FieldMonetary = request.env['ir.qweb.field.monetary']
+        monetary_options = {'display_currency': currency_id}
 
-        # Precio base del template 
         base_price = product.product_tmpl_id.list_price
         res.update({
             'list_price_base_raw': base_price if price_public_visibility else ' ',
