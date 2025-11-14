c250ef734ddb8678ca8bb511769432f4d1bc2404
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-14 16:24:09 -0500

    fix-update: ...

--- theme_prime_image/controllers/main.py
+++ theme_prime_image/controllers/main.py
@@ -34,17 +34,33 @@ class ThemePrimeMainClassExtended(ThemePrimeMainClass):
         res = super()._get_computed_product_price(
             product, product_data, price_public_visibility, visibility_label, currency_id
         )
-
         FieldMonetary = request.env['ir.qweb.field.monetary']
         monetary_options = {'display_currency': currency_id}
-        base_price = product.list_price
+
+        # Corregido: soporta tanto product.template como product.product
+        base_price = product.list_price if product._name == 'product.template' else product.product_tmpl_id.list_price
 
         res.update({
             'list_price_base_raw': base_price if price_public_visibility else ' ',
             'list_price_base': FieldMonetary.value_to_html(base_price, monetary_options) if price_public_visibility else ' '
         })
-
         return res
+
+    # def _get_computed_product_price(self, product, product_data, price_public_visibility, visibility_label, currency_id):
+    #     res = super()._get_computed_product_price(
+    #         product, product_data, price_public_visibility, visibility_label, currency_id
+    #     )
+
+    #     FieldMonetary = request.env['ir.qweb.field.monetary']
+    #     monetary_options = {'display_currency': currency_id}
+    #     base_price = product.list_price
+
+    #     res.update({
+    #         'list_price_base_raw': base_price if price_public_visibility else ' ',
+    #         'list_price_base': FieldMonetary.value_to_html(base_price, monetary_options) if price_public_visibility else ' '
+    #     })
+
+    #     return res
 
 
 
