50417436f8a0d4694154cf54ce7a441cc303ea81
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-11-14 09:47:15 -0500

    fix-update

--- advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
+++ advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 import publicWidget from "@web/legacy/js/public/public_widget";
 import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
+import { utils as uiUtils } from "@web/core/ui/ui_service";
 
 const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
     selector: ".s_dynamic_snippet_categories",
@@ -13,9 +14,48 @@ const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
         return "/shop";
     },
 
+    /**
+     * @override
+     * @private
+     */
+    _getQWebRenderOptions: function () {
+        // obtenemos las opciones originales del padre
+        const options = this._super.apply(this, arguments);
 
+        if (uiUtils.isSmall()) {
+            options.rowPerSlide = 2;  // en móviles ahora serán 2 elementos por slide
+        }
+
+        // en pantallas grandes se mantiene la lógica original
+        return options;
+    },
 });
 
 publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
 
 export default DynamicSnippetCategories;
+
+
+
+
+// /** @odoo-module **/
+// import publicWidget from "@web/legacy/js/public/public_widget";
+// import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
+
+// const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
+//     selector: ".s_dynamic_snippet_categories",
+
+//     /**
+//      * @override
+//      * @private
+//      */
+//     _getMainPageUrl() {
+//         return "/shop";
+//     },
+
+
+// });
+
+// publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
+
+// export default DynamicSnippetCategories;
